95b25e8ff6f1c996cbe17ac46cd71ce70520a28e
YaroDE-ops <yaro.demeulenaere@jopie.be>
2025-02-25 13:24:18 +0100

    Update

--- boekapp/ContentView.swift
+++ boekapp/ContentView.swift
@@ -9,13 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView{
+            home().tabItem{
+                Text("home")
+            }
+            library().tabItem{
+                Text("library")
+            }
+            bookstore().tabItem{
+                Text("bookstore")
+            }
+            audiobooks().tabItem{
+                Text("audiobooks")
+            }
+            search().tabItem{
+                Text("search")
+            }
         }
-        .padding()
+    }
+}
+
+struct home: View {
+    var body: some View {
+        Text("lijst")
+    }
+}
+struct library: View {
+    var body: some View {
+        Text("lijst")
+    }
+}
+struct bookstore: View {
+    var body: some View {
+        Text("lijst")
+    }
+}
+struct audiobooks: View {
+    var body: some View {
+        Text("lijst")
+    }
+}
+struct search: View {
+    @State private var searchText = ""
+        @State private var searchIsActive = false
+    var body: some View {
+        NavigationStack{
+            Text("Searching for \(searchText)")
+            Image(systemName: "microphone")
+                .navigationTitle("search")
+        }
+        .searchable(text: $searchText)
+
+        
     }
 }
 
