ec1ad307fc6f27688e5275ceda6ffd59991fce97
YaroDE-ops <yaro.demeulenaere@jopie.be>
2025-02-25 11:32:26 +0100

    Initial Commit

---
+++ boekapp/boekappApp.swift
@@ -0,0 +1,17 @@
+//
+//  boekappApp.swift
+//  boekapp
+//
+//  Created by yaro demeulenaere on 25/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct boekappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
