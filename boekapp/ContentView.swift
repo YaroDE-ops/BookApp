f53f846bfee9375f49165b3fcad19f45cf6b34b2
YaroDE-ops <yaro.demeulenaere@jopie.be>
2025-02-25 14:05:40 +0100

    update

--- boekapp/ContentView.swift
+++ boekapp/ContentView.swift
@@ -11,18 +11,24 @@ struct ContentView: View {
     var body: some View {
         TabView{
             home().tabItem{
+                Image(systemName:"house.fill")
                 Text("home")
+                
             }
             library().tabItem{
+                Image(systemName:"books.vertical")
                 Text("library")
             }
             bookstore().tabItem{
+                Image(systemName:"bag.fill")
                 Text("bookstore")
             }
             audiobooks().tabItem{
+                Image(systemName:"headphones")
                 Text("audiobooks")
             }
             search().tabItem{
+                Image(systemName:"magnifyingglass")
                 Text("search")
             }
         }
@@ -51,16 +57,23 @@ struct audiobooks: View {
 }
 struct search: View {
     @State private var searchText = ""
-        @State private var searchIsActive = false
+    
     var body: some View {
         NavigationStack{
-            Text("Searching for \(searchText)")
-            Image(systemName: "microphone")
-                .navigationTitle("search")
+            HStack{
+                TextField("books & audiobooks", text: $searchText)
+                    .padding(10)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(10)
+                Button(action: {
+                    print("Microphone tapped")
+                }) {
+                    Image(systemName: "mic.fill")
+                        .foregroundColor(.gray)
+                }
+            }
+            .padding()
         }
-        .searchable(text: $searchText)
-
-        
     }
 }
 
